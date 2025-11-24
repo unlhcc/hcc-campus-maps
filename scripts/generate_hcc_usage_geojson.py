1c3f3caf1af72f13ffccb7aec1dbbd856f27ecda
ldoughty2 <ldoughty2@huskers.unl.edu>
2025-11-24 12:51:49 -0600

    fixed out of bounds error if path is not included, added scrape url to fetch_building_geojson()

--- scripts/generate_hcc_usage_geojson.py
+++ scripts/generate_hcc_usage_geojson.py
@@ -15,15 +15,16 @@ def save_dict_as_json(data_dict: dict, filename: str):
     
     
 def generate_hcc_usage_geojson():
+  SCRAPE_SOURCE_URL = 'https://maps.unl.edu'
   departments_using_hcc = get_and_revise_departments()
   building_names_using_hcc = [get_buildings_from_department(department) for department in departments_using_hcc]
-  buildings_geojson = fetch_building_geojson()
+  buildings_geojson = fetch_building_geojson(SCRAPE_SOURCE_URL)
   for feature in buildings_geojson.features:
     feature.properties["uses_hcc"] = (feature.properties.NAME in building_names_using_hcc)
     
     
 if __name__ == "__main__":
-  output_path = sys.argv[1] or DEFAULT_OUTPUT_PATH
+  output_path = Path(sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1] else DEFAULT_OUTPUT_PATH
   hcc_usage_geojson = generate_hcc_usage_geojson()
   if hcc_usage_geojson:
     save_dict_as_json(hcc_usage_geojson, output_path)
