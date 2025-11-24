7ab66883721550071cc622e26ee497be1f413f05
ldoughty2 <ldoughty2@huskers.unl.edu>
2025-11-24 12:56:50 -0600

    forgot to return the geojson

--- scripts/generate_hcc_usage_geojson.py
+++ scripts/generate_hcc_usage_geojson.py
@@ -14,14 +14,14 @@ def save_dict_as_json(data_dict: dict, filename: str):
     json.dump(data_dict, json_file, indent=2)
     
     
-def generate_hcc_usage_geojson():
+def generate_hcc_usage_geojson() -> dict:
   SCRAPE_SOURCE_URL = 'https://maps.unl.edu'
   departments_using_hcc = get_and_revise_departments()
   building_names_using_hcc = [get_buildings_from_department(department) for department in departments_using_hcc]
   buildings_geojson = fetch_building_geojson(SCRAPE_SOURCE_URL)
   for feature in buildings_geojson["features"]:
     feature["properties"]["uses_hcc"] = (feature["properties"]["NAME"] in building_names_using_hcc)
-    
+  return buildings_geojson
     
 if __name__ == "__main__":
   output_path = Path(sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1] else DEFAULT_OUTPUT_PATH
@@ -29,3 +29,5 @@ if __name__ == "__main__":
   if hcc_usage_geojson:
     save_dict_as_json(hcc_usage_geojson, output_path)
     print(f"HCC usage geojson data saved to {output_path}.")
+  else:
+    print("something went wrong")
